3768186973fde4f467ad08719a244a9fdc319d6a
emirhancakirr <ecakir20@ku.edu.tr>
2025-08-26 18:11:42 +0300

    init

---
+++ src/main/java/com/example/learning_spring_framework/LearningSpringFrameworkApplication.java
@@ -0,0 +1,21 @@
+package com.example.learning_spring_framework;
+
+import com.example.learning_spring_framework.game.GameRunner;
+import com.example.learning_spring_framework.game.MarioGame;
+import com.example.learning_spring_framework.game.SuperContraGame;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearningSpringFrameworkApplication {
+
+	public static void main(String[] args) {
+	// SpringApplication.run(LearningSpringFrameworkApplication.class, args);
+	// MarioGame game = new MarioGame();
+		 SuperContraGame game = new SuperContraGame();
+		GameRunner runner = new GameRunner(game);
+	runner.run();
+		
+	}
+
+}
---
+++ src/main/java/com/example/learning_spring_framework/game/GameRunner.java
@@ -0,0 +1,15 @@
+package com.example.learning_spring_framework.game;
+
+public class GameRunner {
+    private SuperContraGame game;
+    public GameRunner(SuperContraGame game) {
+        this.game = game;
+    }
+
+    public void run(){
+        game.up();
+        game.left();
+        game.right();
+        game.down();
+    }
+}
